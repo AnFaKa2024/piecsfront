8aab96f4096e0e01367be1a51eeb0d07bfb8f8d8
Kare Marques <trabalhosfiap12@gmail.com>
2024-11-22 15:39:07 -0300

    correções no beneficiario

--- src/app/adm/Beneficiario.tsx
+++ src/app/adm/Beneficiario.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { BeneficiarioProps, LocacaoProps } from "@/types";
+import { useState} from "react";
+import { LocacaoProps } from "@/types";
 import { v4 as uuidv4 } from "uuid"
 import { AiOutlinePlusCircle, AiOutlineDelete, AiOutlineSave, AiOutlineEye } from "react-icons/ai";
 
